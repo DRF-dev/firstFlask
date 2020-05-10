da0c4cbbeb015f310884e90dd817eacf5adf8bcf
Dany <dany.francisco@outlook.com>
2020-05-11 01:05:15 +0200

    script montrant des informations sur notre machine

--- index.py
+++ index.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 # -*- coding: Utf-8 -*-
 from flask import Flask, render_template
+import platform
+import netifaces
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    data={'user':"DRF", 'sys':"Unbuntu"}
+    data={
+        'user':"DRF",
+        'machine':platform.node(),
+        'os':platform.system(),
+        'dist':platform.linux_distribution(),
+        'interface':netifaces.interfaces()
+    }
     return render_template('hello.html', name="hello", data=data)
 
 if __name__ == "__main__":
